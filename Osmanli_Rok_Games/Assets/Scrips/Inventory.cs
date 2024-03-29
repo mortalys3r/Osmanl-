57baaf904cb7d8f074f8873af7a0f1b3c18abbca
mortalys3r <yusufay00412@gmail.com>
2024-03-29 19:32:26 +0300

    denemesa

---
+++ Osmanli_Rok_Games/Assets/Scrips/Inventory.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int güvenlik=3, para=3, din = 3, halk=3;
+    //public int sempati;
+
+    public void para_olay(int durum)
+    {
+        if (durum == -1 && para > 0)
+        {
+            para=para-1;
+        }
+        else if (durum == 1 && para < 10)
+        {
+            para = para + 1;
+        }
+    }
+    public void güvenlik_olay(int durum)
+    {
+        if (durum == -1 && güvenlik > 0)
+        {
+            güvenlik = güvenlik - 1;
+        }
+        else if (durum == 1 && güvenlik < 10)
+        {
+            güvenlik = güvenlik + 1;
+        }
+    }
+    public void halk_olay(int durum)
+    {
+        if (durum == -1 && halk > 0)
+        {
+            halk = halk - 1;
+        }
+        else if (durum == 1 && halk < 10)
+        {
+            halk= halk + 1;
+        }
+    }
+    public void din_olay(int durum)
+    {
+        if (durum == -1 && din >0)
+        {
+            din= din - 1;
+        }
+        else if (durum == 1 && din < 10)
+        {
+            din = din + 1;
+        }
+    }
+}
