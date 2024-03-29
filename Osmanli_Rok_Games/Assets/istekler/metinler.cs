315f313d5e83673df51a3b455c63802c44d4e1dd
mortalys3r <yusufay00412@gmail.com>
2024-03-29 21:40:26 +0300

    güncel

--- Osmanli_Rok_Games/Assets/istekler/metinler.cs
+++ Osmanli_Rok_Games/Assets/istekler/metinler.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class metinler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+/*
+ * Ýmam: Dini bütünlüðümüzü arttýrmak için yeni bir cami yapýlmasýný talep ediyorum...
+ * 
+ * Doktor : Hastalýklarý hýzlýca çözmek ve yaralý askerlerimizi kurtarmak için bir hastane yapýlmasýný talep ediyorum...
+ * 
+ * halk : vergileri azaltmanýzý talep ediyorum...
+ * 
+ * Valide Sultan : Þehzadem için münasip bir eþ adayý bulduðuma inanýyorum.
+ * 
+ * Paþa : Hünkarým, padiþahlýðýmýza baðlý ve sadýk ordunuzu en iyi þekilde eðitebilmem ve ordumuzun ihtiyaç duyduðu silahlarý temin edebilmem için hazineden mali yardým talep ediyorum. 
+ * 
+ * Nefer : hazineyi koruma görevine nail edilme talebinde bulunuyorum...
+ * 
+ * onbaþý : Hünkarým sarayýn güvenliðini arttýrmayý talep ediyorum... 
+ * 
+ * yaya : Dýþarýda sizi görmek isteyen birisi ba
+ * 
+ * 
+ * 
+ */
