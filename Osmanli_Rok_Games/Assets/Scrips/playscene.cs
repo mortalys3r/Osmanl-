c1a9708e615ace57078220c9caaf18e9a8fd5276
mortalys3r <yusufay00412@gmail.com>
2024-03-31 13:01:33 +0300

    veri

--- Osmanli_Rok_Games/Assets/Scrips/playscene.cs
+++ Osmanli_Rok_Games/Assets/Scrips/playscene.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
+
 
 public class playscene : MonoBehaviour
 {
@@ -9,20 +9,11 @@ public class playscene : MonoBehaviour
     public GameObject credits_menu;
     public GameObject set_menu;
     public GameObject game_seviye_menu;
-    public int seviye;
+
 
     public void Start()
     {
-        Veri data = new Veri();
-        data.sevi = seviye;
-
-        string jsonData = JsonUtility.ToJson(data);
-        File.WriteAllText(Application.dataPath + "/data.json", jsonData);
-
-
-
-
-
+        game_seviye_menu.SetActive(false);
         credits_menu.SetActive(false);
         main_menu.SetActive(true);
         set_menu.SetActive(false);
@@ -31,18 +22,9 @@ public class playscene : MonoBehaviour
 
 
 
-
-    public void veritabani()
-    {
-        Veri data = new Veri();
-        data.sevi = seviye;
-
-        string jsonData = JsonUtility.ToJson(data);
-        File.WriteAllText(Application.dataPath + "/data.json", jsonData);
-    }
-
     public void credits()
     {
+        game_seviye_menu.SetActive(false);
         credits_menu.SetActive(true);
         main_menu.SetActive(false);
         set_menu.SetActive(false);
@@ -50,6 +32,7 @@ public class playscene : MonoBehaviour
 
     public void setting_menu()
     {
+        game_seviye_menu.SetActive(false);
         credits_menu.SetActive(false);
         main_menu.SetActive(false);
         set_menu.SetActive(true);
@@ -57,6 +40,7 @@ public class playscene : MonoBehaviour
 
     public void Main_Menu()
     {
+        game_seviye_menu.SetActive(false);
         credits_menu.SetActive(false);
         main_menu.SetActive(true);
         set_menu.SetActive(false);
@@ -70,17 +54,4 @@ public class playscene : MonoBehaviour
         game_seviye_menu.SetActive(true);
     }
 
-
-    public void seviyelisecim(int sayi)
-    {
-        seviye = sayi;
-    }
-
-
-
-    [System.Serializable]
-    public class Veri
-    {
-        public int sevi; // veya baþka bir veri türü
-    }
 }
